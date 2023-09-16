0bcfe5a205a49466d7704d9591d3473f9f56e8f5
this-is-yaash <yashwanthrathakrishnan@gmail.com>
2023-09-16 15:31:12 +0530

    modularization FAB&eFAB with exit #3

--- app/src/main/java/com/example/prototype_alpha/MainActivity.java
+++ app/src/main/java/com/example/prototype_alpha/MainActivity.java
@@ -46,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
             }
         });
-
         fab2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
